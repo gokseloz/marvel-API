6cd97d25173d4808f682f924a6f515ef03e6ff22
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-05-07 22:36:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const api = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f3590148aa33d73160c34cf4a712fac8&hash=0d90061027af6621ca73185cef650d56&limit=50&offset=0`;
 
-let ignores = ["http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708"];
-
 let footer = document.querySelector(".footer");
 let container = document.querySelector(".container");
 
